fd0fe8bdcf7e2cb6f0c0ca4efaac05f7c7fccceb
takutyamo <59073115+takutyamo@users.noreply.github.com>
2021-07-06 22:56:14 +0900

    css追加

--- app/helpers/employee_helper.rb
+++ app/helpers/employee_helper.rb
@@ -1,2 +1,9 @@
-module EmployeeHelper  
+module EmployeeHelper
+    def admin(admin)
+        if admin
+            "管理者"
+        else
+            "申請者"
+        end
+    end  
 end
--- app/helpers/expense_approvals_helper.rb
+++ app/helpers/expense_approvals_helper.rb
@@ -1,2 +1,5 @@
 module ExpenseApprovalsHelper
+    def date_format(appdate)
+        appdate.strftime('%Y年%m月%d日')
+    end
 end
